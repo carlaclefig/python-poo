cae01a61b5cd462b8c281a13b279a9bd304b9ec6
Carla Clemente <carlaclefig@gmail.com>
2024-07-04 17:32:17 -0500

    feat: use modena exchange rate

---
+++ 3.use_tipo_cambio.py
@@ -0,0 +1,7 @@
+from tipo_cambio import *  # pylint: disable=wildcard-import
+
+soles = ingresar_valor()
+
+print("Monto en dolares: ", tipo_dolar(soles))
+print("Monto en euros: ", tipo_euro(soles))
+print("Monto en yen: ", tipo_yen(soles))
