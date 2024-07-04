f027203a924b49db7b47eaf1b4e14a63ca72f60f
Carla Clemente <carlaclefig@gmail.com>
2024-07-04 13:50:27 -0500

    chore: add operation math 1

---
+++ modulos/math_1.py
@@ -0,0 +1,14 @@
+def addition(n1, n2):
+    return n1 + n2
+
+
+def subtraction(n1, n2):
+    return n1 - n2
+
+
+def multiplication(n1, n2):
+    return n1 * n2
+
+
+def division(n1, n2):
+    return round(n1 / n2, 2)
