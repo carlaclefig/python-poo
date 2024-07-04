485524970bfe554e28e002a6fb57819234ecbf78
Carla Clemente <carlaclefig@gmail.com>
2024-07-04 13:55:20 -0500

    feat: add basic math operations

---
+++ 1.use_math.py
@@ -0,0 +1,9 @@
+from modulos.math_1 import *  # pylint: disable=wildcard-import
+
+a = 20
+b = 10
+
+print("Suma: ", addition(a, b))
+print("Resta: ", subtraction(a, b))
+print("Multiplicación: ", multiplication(a, b))
+print("División: ", division(a, b))
