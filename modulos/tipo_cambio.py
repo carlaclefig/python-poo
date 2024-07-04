3642d55fca1819f625a9b635f7696093135e5bfd
Carla Clemente <carlaclefig@gmail.com>
2024-07-04 17:38:03 -0500

    moved

---
+++ modulos/tipo_cambio.py
@@ -0,0 +1,14 @@
+def ingresar_valor():
+    return float(input("Valor monetario a convertir de soles: "))
+
+
+def tipo_dolar(valor):
+    return round(valor / 3.84, 2)
+
+
+def tipo_euro(valor):
+    return round(valor / 4.08, 2)
+
+
+def tipo_yen(valor):
+    return round(valor / 0.024, 2)
